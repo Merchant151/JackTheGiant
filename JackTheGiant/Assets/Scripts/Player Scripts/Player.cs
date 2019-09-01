fc4eb63b8f6fb81ce1aa5fb821287d07b2befe17
Joshua "Mart" Miller <32748201+Merchant151@users.noreply.github.com>
2019-08-31 21:07:38 -0400

    Adds annimation and changes sprite directionIMBACK

--- JackTheGiant/Assets/Scripts/Player Scripts/Player.cs
+++ JackTheGiant/Assets/Scripts/Player Scripts/Player.cs
@@ -6,9 +6,9 @@ public class Player : MonoBehaviour
 {
     public float speed = 8f, maxVelocity = 4f;
 
+    //[SerializeField]
     private Rigidbody2D myBody;
     private Animator anim;
-
     void Awake ()
     {
         myBody = GetComponent<Rigidbody2D>();
@@ -17,7 +17,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Player Script has started");
     }
 
     // Update is called once per frame
@@ -31,16 +31,37 @@ public class Player : MonoBehaviour
         float vel = Mathf.Abs ( myBody.velocity.x);
 
         float h = Input.GetAxisRaw ("Horizontal");
-
-        if(h > 0)
+        Vector3 temp = transform.localScale;
+        if (h > 0)
         {
+            //Debug.Log("right movment detected");
             if (vel < maxVelocity)
                 forceX = speed;
-        }else if(h < 0)
+
+            //seting the scale makes sure the sprite is facing the correct direction
+            temp.x = 1.3f;
+            transform.localScale = temp;
+
+            //starts walk animation
+            anim.SetBool("Walk", true);
+        }
+        else if (h < 0)
         {
+            //Debug.Log("left movment detected");
             if (vel < maxVelocity)
                 forceX = -speed;
+
+            //set scale backwards
+            temp.x = -1.3f;
+            transform.localScale = temp;
+
+            anim.SetBool("Walk", true);
+
+        } else
+        {
+            anim.SetBool("Walk", false);
         }
+        
 
         myBody.AddForce(new Vector2(forceX, 0));
     }
