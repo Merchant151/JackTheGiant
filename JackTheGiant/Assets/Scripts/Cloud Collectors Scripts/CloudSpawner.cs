7204499efb6f1e7ab4daa877f062e17915abfb46
Joshua "Mart" Miller <32748201+Merchant151@users.noreply.github.com>
2019-09-22 17:33:15 -0400

    Positioned player

--- JackTheGiant/Assets/Scripts/Cloud Collectors Scripts/CloudSpawner.cs
+++ JackTheGiant/Assets/Scripts/Cloud Collectors Scripts/CloudSpawner.cs
@@ -23,8 +23,16 @@ public class CloudSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
+        controlX = 0;
         SetMinAndMaxX();
         CreateClouds();
+        player = GameObject.Find("Player");
+        
+    }
+
+    private void Start()
+    {
+        PositionThePlayer();
     }
 
     void SetMinAndMaxX()
@@ -32,8 +40,8 @@ public class CloudSpawner : MonoBehaviour
         Vector3 bounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
 
         maxX = bounds.x - 1f;
-        minX = -bounds.x + 1.1f;
-        Debug.Log("maxX = " + maxX + "minX = " + minX);
+        minX = -bounds.x + 1f;
+        //Debug.Log("maxX = " + maxX + "minX = " + minX);
     }
     void Shuffle(GameObject[] arrayToShuffle)
     {
@@ -60,13 +68,13 @@ public class CloudSpawner : MonoBehaviour
             //spawns clouds in starcase fassion
             if (controlX == 0)
             {
-                temp.x = Random.Range(0.0f, 1f);
+                temp.x = Random.Range(0.0f, maxX);
                 controlX = 1;
                 //Debug.Log("controlX = " + controlX);
             }
             else if (controlX == 1)
             {
-                temp.x = Random.Range(0.0f, -1f);
+                temp.x = Random.Range(0.0f, minX);
                 controlX = 2;
                 //Debug.Log("controlX = " + controlX);
             }
@@ -87,6 +95,41 @@ public class CloudSpawner : MonoBehaviour
             clouds[i].transform.position = temp;
             positionY -= distanceBetween;
         }
+    }
+
+    void PositionThePlayer()
+    {
+        GameObject[] darkClouds = GameObject.FindGameObjectsWithTag("Deadly");
+        GameObject[] cloudsInGame = GameObject.FindGameObjectsWithTag("Cloud");
+
+        for(int i = 0; i < darkClouds.Length; i++)
+        {
+
+            if(darkClouds[i].transform.position.y == 0f)
+            {
+                Vector3 t = darkClouds[i].transform.position;
+                darkClouds[i].transform.position = new Vector3(cloudsInGame[0].transform.position.x,
+                                                               cloudsInGame[0].transform.position.y, 
+                                                               cloudsInGame[0].transform.position.z);
+                cloudsInGame[0].transform.position = t;
+            }
+
+            
+        }
+
+        Vector3 temp = cloudsInGame[0].transform.position;
+
+        for(int i = 1; i < cloudsInGame.Length; i++)
+        {
+            if(temp.y < cloudsInGame[i].transform.position.y)
+            {
+                temp = cloudsInGame[i].transform.position;
+            }
+        }
+
+        temp.y += 0.8f;
+
+        player.transform.position = temp;
     }
     // Update is called once per frame
     void Update()
